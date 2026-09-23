7d290ada73f7d781ec80de681de3677f1ddf1e26
iamashishkundu <ashishkundu1708@gmail.com>
2026-09-23 10:45:34 +0530

    day-14 updated

--- Day-14/Practical/01.js
+++ Day-14/Practical/01.js
@@ -8,30 +8,57 @@ const pool = await mysql.createPool({
     connectionLimit: 10,
 });
 
-try{
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
-            email VARCHAR(255) NOT NULL
-        ) 
-        `);
-        console.log(`Table created succesfully.`)
-
-    await pool.query(`
-        INSERT INTO users (name,email)
+async function insert(){
+     await pool.query(`
+        INSERT INTO users (id,name,email)
         VALUES
-            ('Ashish','ashish@gmail.com'),
-            ('Aryan','aryan@gmail.com'),
-            ('Aditya','aditya@gmail.com')
+            ('1','Ashish','ashish@gmail.com'),
+            ('2','Aryan','aryan@gmail.com'),
+            ('3','Aditya','aditya@gmail.com')
         `);
+    console.log(`Data inserted Successfully.`);
+}
 
-    const [rows] = await pool.query(`
+async function print(){
+     const [rows] = await pool.query(`
         SELECT * FROM users
         `);
 
     console.log(`Users:`);
     console.log(rows);
+}
+
+async function remove(){
+    await pool.query(`
+        DELETE FROM users WHERE id = 2
+        `);
+    console.log(`Data Deleted Successfully.`)
+}
+
+async function modify(){
+    await pool.query(`
+        UPDATE users
+        SET name = 'Adi'
+        WHERE id = 3
+        `)
+    console.log(`Data Modified successfully.`)
+}
+try{
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INT PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(255) NOT NULL
+        ) 
+        `);
+        console.log(`Table created succesfully.`)
+        await insert();
+        await print();
+        await modify();
+        await print();
+        await remove();
+        await print();
+
 
 }catch(err){
     console.log("Error:",err);
